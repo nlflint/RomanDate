5cdee53ee3e952c7d9c901a04c759f2ad7bda2b4
Nathan Flint <nlflint@gmail.com>
2019-04-13 16:13:30 -0700

    Put dictionaries into a list

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -34,55 +34,58 @@ namespace ClassLibrary1
             Assert.Equal(ToRoman(999), "CMXCIX");
 
         }
-
-        private Dictionary<int, string> Ones = new Dictionary<int, string>
+        
+        private List<Dictionary<int, string>> RomanByPowers = new List<Dictionary<int, string>>
         {
-            {0,"" },
-            {1,"I" },
-            {2,"II" },
-            {3,"III" },
-            {4,"IV" },
-            {5,"V" },
-            {6,"VI" },
-            {7,"VII" },
-            {8,"VIII" },
-            {9,"IX" },
+            new Dictionary<int, string>
+            {
+                {0,"" },
+                {1,"I" },
+                {2,"II" },
+                {3,"III" },
+                {4,"IV" },
+                {5,"V" },
+                {6,"VI" },
+                {7,"VII" },
+                {8,"VIII" },
+                {9,"IX" },
+            },
+            new Dictionary<int, string>
+            {
+                {0,"" },
+                {1,"X" },
+                {2,"XX" },
+                {3,"XXX" },
+                {4,"XL" },
+                {5,"L" },
+                {6,"LX" },
+                {7,"LXX" },
+                {8,"LXXX" },
+                {9,"XC" },
+            },
+            new Dictionary<int, string>
+            {
+                {0,"" },
+                {1,"C" },
+                {2,"CC" },
+                {3,"CCC" },
+                {4,"CD" },
+                {5,"D" },
+                {6,"DC" },
+                {7,"DCC" },
+                {8,"DCCC" },
+                {9,"CM" },
+            }
         };
-
-        private Dictionary<int, string> Tens = new Dictionary<int, string>
-        {
-            {0,"" },
-            {1,"X" },
-            {2,"XX" },
-            {3,"XXX" },
-            {4,"XL" },
-            {5,"L" },
-            {6,"LX" },
-            {7,"LXX" },
-            {8,"LXXX" },
-            {9,"XC" },
-        };
-
-        private Dictionary<int, string> Hundreds = new Dictionary<int, string>
-        {
-            {0,"" },
-            {1,"C" },
-            {2,"CC" },
-            {3,"CCC" },
-            {4,"CD" },
-            {5,"D" },
-            {6,"DC" },
-            {7,"DCC" },
-            {8,"DCCC" },
-            {9,"CM" },
-        };
-
+        
         private string ToRoman(int number)
         {
-
-            return Hundreds[DigitAtPosition(number, 2)] +
-                Tens[DigitAtPosition(number, 1)] +
-                Ones[DigitAtPosition(number, 0)];
+            return Enumerable
+                .Range(0,3)
+                .Aggregate("", (acc, power) => {
+                    var powerSet = RomanByPowers[power];
+                    return powerSet[DigitAtPosition(number, power)] + acc;
+            });
         }
 
         private static int DigitAtPosition(int number, int position)
