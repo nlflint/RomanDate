ff681a95243218ba3a9a596a84a42de0e0d523b5
Nathan Flint <nlflint@gmail.com>
2019-04-15 00:23:46 -0700

    Finished moving away from dictionaries

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -10,7 +10,7 @@ namespace ClassLibrary1
     public class Roman
     {
         [Fact]
-        public void Test()
+        public void NumberToRomanTests()
         {
             Assert.Equal("I", ToRoman(1));
             Assert.Equal("II", ToRoman(2));
@@ -35,100 +35,55 @@ namespace ClassLibrary1
 
         }
         
-        private List<Dictionary<int, string>> RomanSetByMagnitude = new List<Dictionary<int, string>>
-        {
-            new Dictionary<int, string>
-            {
-                {0,"" },
-                {1,"I" },
-                {2,"II" },
-                {3,"III" },
-                {4,"IV" },
-                {5,"V" },
-                {6,"VI" },
-                {7,"VII" },
-                {8,"VIII" },
-                {9,"IX" },
-            },
-            new Dictionary<int, string>
-            {
-                {0,"" },
-                {1,"X" },
-                {2,"XX" },
-                {3,"XXX" },
-                {4,"XL" },
-                {5,"L" },
-                {6,"LX" },
-                {7,"LXX" },
-                {8,"LXXX" },
-                {9,"XC" },
-            },
-            new Dictionary<int, string>
-            {
-                {0,"" },
-                {1,"C" },
-                {2,"CC" },
-                {3,"CCC" },
-                {4,"CD" },
-                {5,"D" },
-                {6,"DC" },
-                {7,"DCC" },
-                {8,"DCCC" },
-                {9,"CM" },
-            }
-        };
-        
-        
-        private List<string> Denominations = new [] {"I","V","X","L","C","D","M"}.ToList();
         private string ToRoman(int number)
-        {
-            var digitsByMagnitude = BreakIntoDigits(number);
+        {   
+            var denominationSetfactory = new DenominationSetFactory();
 
-            var denominationsByMagnitude = Enumerable
-                .Range(0, (Denominations.Count - 1) / 2)
-                .Select(magnitude => new DenominationSet(Denominations.Skip(2 * magnitude).Take(3).ToList()));
-
-                var blah = denominationsByMagnitude.Skip(1).First();
-                Console.WriteLine("num: ",  blah.Small);
-                // .Zip(digitsByMagnitude, DigitToRoman)
-                // .Aggregate(string.Concat);
-
-            return RomanSetByMagnitude
-                .Select(
-                    (romanSet, idx) => romanSet[DigitAtPosition(number, idx)])
-                .Reverse()    
+            return Enumerable
+                .Range(0, denominationSetfactory.SupportedPowersOfTenCount)
+                .Select(denominationSetfactory.Make)
+                .Zip(BreakIntoDigits(number), ConvertDigitToRoman)
+                .Reverse()
                 .Aggregate(string.Concat);
-
-            // return Enumerable
-            //     .Range(0,3)
-            //     .Aggregate("", (acc, power) => {
-            //         var powerSet = RomanSetByMagnitude[power];
-            //         return powerSet[DigitAtPosition(number, power)] + acc;
-            // });
-        }
-
-        private string DigitToRoman(DenominationSet set, int digit) {
-            if (digit == 0) return "";
-                    if (digit < 4) return new String(set.Small.First(),digit);
-                    if (digit == 4) return set.Small + set.Medium;
-                    if (digit == 5) return set.Medium;
-                    if (digit < 9) return set.Medium + new String(set.Small.First(),digit - 5);
-                    return set.Medium + set.Large;
         }
         private IEnumerable<int> BreakIntoDigits(int number) {
-            while(number > 0) {
+            do {
                 yield return number  % 10;
                 number /= 10;
-            }
+            } while (number > 0);
         }
 
-        private string ConvertOrderOfMagnitude(List<string> Denominations, int magnitude) {
-            return "asdf";
+        private string ConvertDigitToRoman(DenominationSet set, int digit) {
+            if (digit == 0) return "";
+            if (digit < 4) return new String(set.Small.First(),digit);
+            if (digit == 4) return set.Small + set.Medium;
+            if (digit == 5) return set.Medium;
+            if (digit < 9) return set.Medium + new String(set.Small.First(),digit - 5);
+            return set.Small + set.Large;
         }
-        private static int DigitAtPosition(int number, int position)
+    }
+
+    public class DenominationSetFactory {
+        private List<string> _denominations = new [] {"I","V","X","L","C","D","M"}.ToList();
+        public DenominationSet Make(int powerOfTen) {
+            var subSet = _denominations.Skip(2 * powerOfTen).Take(3).ToList();
+            return new DenominationSet(subSet[0], subSet[1], subSet[2]);
+        }
+
+        public int SupportedPowersOfTenCount => (_denominations.Count - 1) / 2;
+    }
+
+    public class DenominationSet {
+        public DenominationSet(string small, string medium, string large)
         {
-            var asdf = (int) Math.Pow(10,position);
-            return number / asdf % 10;
+            
+            Small = small;
+            Medium = medium;
+            Large = large;
         }
+
+        public string Small {get;}
+        public string Medium {get;}
+        public string Large {get;}
     }
 }
