2a4575d89ad20c82b28e4059fae7ec7d72792c91
Nathan Flint <nlflint@gmail.com>
2019-04-15 09:01:03 -0700

    Using array lookup and zip!

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace ClassLibrary1
 {
     public class Roman
     {
+        private readonly ITestOutputHelper output;
+
+        public Roman(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
         [Fact]
         public void Test()
         {
@@ -33,65 +41,39 @@ namespace ClassLibrary1
             Assert.Equal("CMLXX", ToRoman(970));
             Assert.Equal("CMXCIX", ToRoman(999));
 
+            Assert.Equal("M", ToRoman(1000));
+            Assert.Equal("MM", ToRoman(2000));
+            Assert.Equal("MMM", ToRoman(3000));
+            Assert.Equal("IV", ToRoman(4000));
+            Assert.Equal("V", ToRoman(5000));
+            Assert.Equal("VI", ToRoman(6000));
+            Assert.Equal("VII", ToRoman(7000));
+            Assert.Equal("VIII", ToRoman(8000));
+            Assert.Equal("IX", ToRoman(9000));
+            Assert.Equal("IXCMXCIX", ToRoman(9999));
         }
-        
-        private List<Dictionary<int, string>> RomanByPowers = new List<Dictionary<int, string>>
+
+        private string[][] _numeralsByPowersOfTen =
         {
-            new Dictionary<int, string>
-            {
-                {0,"" },
-                {1,"I" },
-                {2,"II" },
-                {3,"III" },
-                {4,"IV" },
-                {5,"V" },
-                {6,"VI" },
-                {7,"VII" },
-                {8,"VIII" },
-                {9,"IX" },
-            },
-            new Dictionary<int, string>
-            {
-                {0,"" },
-                {1,"X" },
-                {2,"XX" },
-                {3,"XXX" },
-                {4,"XL" },
-                {5,"L" },
-                {6,"LX" },
-                {7,"LXX" },
-                {8,"LXXX" },
-                {9,"XC" },
-            },
-            new Dictionary<int, string>
-            {
-                {0,"" },
-                {1,"C" },
-                {2,"CC" },
-                {3,"CCC" },
-                {4,"CD" },
-                {5,"D" },
-                {6,"DC" },
-                {7,"DCC" },
-                {8,"DCCC" },
-                {9,"CM" },
-            }
+            new[] {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+            new[] {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+            new[] {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+            new[] {"", "M", "MM", "MMM", "IV", "V", "VI", "VII", "VIII", "IX"}
         };
         
         private string ToRoman(int number)
         {
-            return Enumerable
-                .Range(0,3)
-                .Aggregate("", (acc, power) => {
-                    var powerSet = RomanByPowers[power];
-                    return powerSet[DigitAtPosition(number, power)] + acc;
-            });
+            return BreakIntoDigits(number)
+                .Zip(_numeralsByPowersOfTen, (digit, numerals) => numerals[digit])
+                .Reverse()
+                .Aggregate(string.Concat);
         }
 
-        private static int DigitAtPosition(int number, int position)
+        private IEnumerable<int> BreakIntoDigits(int number)
         {
-            var asdf = (int) Math.Pow(10,position);
-            return number / asdf % 10;
+            if (number == 0) return new List<int> {0};
+
+            return new List<int> {number % 10 }.Concat(BreakIntoDigits(number / 10));
         }
     }
 }
